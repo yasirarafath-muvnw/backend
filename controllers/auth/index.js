cde2f16ead6ef98d93b96eafcf8bd2b8814a45bc
yasir <yasir.arafath.munvw@gmail.com>
2025-07-28 18:04:19 +0530

    signup controller

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,19 +2,18 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../../models/User/index.js';
 
-const saltRounds = 10;
-const jwtSecret = "00000000";
 
 export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    return res.json({ success: false, message: 'User already exists' });
+  }
+
   try {
-    const { name, email, password } = req.body;
-
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = new User({
       name,
@@ -35,6 +34,7 @@ export const signup = async (req, res) => {
 
     res.status(201).json({
       message: 'User registered successfully',
+      success: true,
       accessToken,
       user: {
         id: newUser._id,
@@ -45,7 +45,8 @@ export const signup = async (req, res) => {
 
   } catch (err) {
     console.error('Signup failed error:', err);
-    res.status(500).json({ message: 'Signup failed', error: err.message });
+
+    res.json({ success: false, error: err.message });
   }
 };
 

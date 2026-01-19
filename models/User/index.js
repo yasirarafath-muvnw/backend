62e77c90bbeae23d72a31eb81d36b79ef95e2f7d
Yasir Arafath <mohammed.yasir@enervoir.in>
2026-01-19 17:53:38 +0530

    'user roles added 'SUPER_ADMIN', 'ADMIN', 'USER''

--- models/User/index.js
+++ models/User/index.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role: { type: String, enum: ['SUPER_ADMIN', 'ADMIN', 'USER'], default: 'USER' }
 }, { timestamps: true });
 
 export default mongoose.model('User', userSchema);
